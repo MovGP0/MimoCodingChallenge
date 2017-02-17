135ca822ec705c8ec4a74ba39252bb2191028c76
Johann Dirry <movgp0@gmail.com>
2017-02-17 10:07:28 +0100

    adding missing connection strings

--- Mimo.Backend/Courses/CoursesContext.cs
+++ Mimo.Backend/Courses/CoursesContext.cs
@@ -5,5 +5,10 @@ namespace Mimo.Backend.Courses
     public class CoursesContext : DbContext
     {
         public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./Courses.db");
+        }
     }
 }
--- Mimo.Backend/Users/UsersContext.cs
+++ Mimo.Backend/Users/UsersContext.cs
@@ -5,5 +5,10 @@ namespace Mimo.Backend.Users
     public class UsersContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./Users.db");
+        }
     }
 }
