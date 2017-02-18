99fd76ee6ed31c9b258ac0b2334b2c87e24423cf
Johann Dirry <movgp0@gmail.com>
2017-02-18 13:51:12 +0100

    fixing unit tests

--- Mimo.Backend.Courses.Tests/CoursesContextTests.cs
+++ Mimo.Backend.Courses.Tests/CoursesContextTests.cs
@@ -1,18 +1,60 @@
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Mimo.Backend.Courses.Tests
 {
     public sealed class CoursesContextTests : AppSettingsTest
-    { 
-        [Fact]
-        public async Task CreateCoursesContextDatabase()
+    {
+        public CoursesContextTests()
+        {
+            using (var cts = new CancellationTokenSource(1000))
+            {
+                SetupAsync(cts.Token).Wait();
+            }
+        }
+
+        private async Task SetupAsync(CancellationToken cancellationToken)
         {
             var connectionString = Configuration["CoursesConnectionString"];
             using (var context = new CoursesContext(connectionString))
             {
-                var success = await context.Database.EnsureCreatedAsync();
-                Assert.True(success);
+                await context.Database.EnsureCreatedAsync(cancellationToken);
+                context.Courses.RemoveRange(context.Courses);
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        }
+
+        [Fact]
+        public async Task CreateCoursesContextDatabase()
+        {
+            var cancellationToken = CancellationToken.None;
+            var connectionString = Configuration["CoursesConnectionString"];
+            using (var context = new CoursesContext(connectionString))
+            {
+                await context.Courses.AddAsync(new Course {Name = "Foo"}, cancellationToken);
+                await context.SaveChangesAsync(cancellationToken);
+                var numberOfCourses = await context.Courses.CountAsync(cancellationToken);
+
+                Assert.Equal(1, numberOfCourses);
+            }
+        }
+
+        ~CoursesContextTests()
+        {
+            using (var cts = new CancellationTokenSource(1000))
+            {
+                TearDownAsync(cts.Token).Wait();
+            }
+        }
+
+        private async Task TearDownAsync(CancellationToken cancellationToken)
+        {
+            var connectionString = Configuration["CoursesConnectionString"];
+            using (var context = new CoursesContext(connectionString))
+            {
+                await context.Database.EnsureDeletedAsync(cancellationToken);
             }
         }
     }
--- Mimo.Backend.Users.Tests/UsersContextTests.cs
+++ Mimo.Backend.Users.Tests/UsersContextTests.cs
@@ -1,18 +1,68 @@
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Mimo.Backend.Users.Tests
 {
     public sealed class UsersContextTests : AppSettingsTest
     {
+        public UsersContextTests()
+        {
+            using (var cts = new CancellationTokenSource(1000))
+            {
+                SetupAsync(cts.Token).Wait();
+            }
+        }
+
+        private async Task SetupAsync(CancellationToken cancellationToken)
+        {
+            var connectionString = Configuration["CoursesConnectionString"];
+            using (var context = new UsersContext(connectionString))
+            {
+                await context.Database.EnsureCreatedAsync(cancellationToken);
+                context.Users.RemoveRange(context.Users);
+                await context.SaveChangesAsync(cancellationToken);
+            }
+        }
+
         [Fact]
         public async Task CreateUsersContextDatabase()
         {
-            var connectionString = Configuration["UsersConnectionString"];
+            using (var cts = new CancellationTokenSource(1000))
+            {
+                var connectionString = Configuration["UsersConnectionString"];
+                using (var context = new UsersContext(connectionString))
+                {
+                    var user = new User
+                    {
+                        Name = "Fred"
+                    };
+
+                    await context.Users.AddAsync(user, cts.Token);
+
+                    await context.SaveChangesAsync(cts.Token);
+                    var numberOfUsers = await context.Users.CountAsync(cts.Token);
+
+                    Assert.Equal(1, numberOfUsers);
+                }
+            }
+        }
+
+        ~UsersContextTests()
+        {
+            using (var cts = new CancellationTokenSource(1000))
+            {
+                TearDownAsync(cts.Token).Wait();
+            }
+        }
+
+        private async Task TearDownAsync(CancellationToken cancellationToken)
+        {
+            var connectionString = Configuration["CoursesConnectionString"];
             using (var context = new UsersContext(connectionString))
             {
-                var success = await context.Database.EnsureCreatedAsync();
-                Assert.True(success);
+                await context.Database.EnsureDeletedAsync(cancellationToken);
             }
         }
     }
